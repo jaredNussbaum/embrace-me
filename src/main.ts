f0741ce6d02c0aa1160dccc37cc2ad4b9b0e04ae
Tate Maguire <tatemaguir@gmail.com>
2025-11-26 23:45:36 -0800

    actually no now its cannon physics engine AND I updated the demo

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as CANNON from "cannon-es";
 import * as THREE from "three";
 import "./style.css";
 
@@ -16,17 +17,32 @@ renderer.setSize(viewportWidth, viewportHeight);
 const canvas = renderer.domElement;
 document.body.appendChild(canvas);
 
-// Make Cube
+// Make Three.js Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xaa00aa });
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 
+// SEPARATE Cannon physics setup
+const world = new CANNON.World();
+
+// Make cannon.js rigidbody for the cube
+const shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
+const body = new CANNON.Body({ mass: 1 });
+body.addShape(shape);
+body.angularVelocity.set(0, 10, 0);
+body.angularDamping = 0.5;
+world.addBody(body);
+
 // Game Loop
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // adjust rendered cube to match the rigidbody
+  cube.position.copy(body.position);
+  cube.quaternion.copy(body.quaternion);
+
+  world.fixedStep();
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
-renderer.setAnimationLoop(animate);
+animate();
