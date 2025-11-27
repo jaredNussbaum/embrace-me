82c53f1880491a2a0f4ece2fa168a077a5be41ae
Tate Maguire <tatemaguir@gmail.com>
2025-11-26 17:48:22 -0800

    installed three and oimo, changed deno formatting

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import * as THREE from "three";
 import "./style.css";
 
 document.body.innerHTML = `
   <h1>EMBRACE ME</h1>
 `;
+
+// deno-lint-ignore no-unused-vars
+const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, globalThis.innerWidth / globalThis.innerHeight);
