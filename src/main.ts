bd0ee40e1fcd62176aa518553b9179d68d7c1959
Tate Maguire <tatemaguir@gmail.com>
2025-11-26 19:36:26 -0800

    setup the three.js demo scene from the three.js manual

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import * as THREE from "three";
 import "./style.css";
 
-document.body.innerHTML = `
-  <h1>EMBRACE ME</h1>
-`;
+// Screen Size Constants
+const viewportWidth = globalThis.innerWidth;
+const viewportHeight = globalThis.innerHeight;
 
-// deno-lint-ignore no-unused-vars
+// Three.js setup
 const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(75, globalThis.innerWidth / globalThis.innerHeight);
+const camera = new THREE.PerspectiveCamera(75, viewportWidth / viewportHeight);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(viewportWidth, viewportHeight);
+
+const canvas = renderer.domElement;
+document.body.appendChild(canvas);
+
+// Make Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xaa00aa });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+// Game Loop
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
